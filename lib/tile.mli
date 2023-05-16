275a2b6735ae0ad2579e793333de208a08c468c7
polybeandip <akashdhiraj2019@gmail.com>
2023-05-15 23:30:03 -0400

    More documentation

--- lib/tile.mli
+++ lib/tile.mli
@@ -1,8 +1,8 @@
 val solid : int -> bool
-(* [solid n] is true if tile n is solid and false otherwise *)
+(** [solid n] is true if tile n is solid and false otherwise *)
 
 val recharge : int -> bool
-(* [recharge n] is true if tile n is a battery and false otherwise *)
+(** [recharge n] is true if tile n is a battery and false otherwise *)
 
 val spike : int -> bool
-(* [spike n] is true if tile n is a spike and false otherwise *)
+(** [spike n] is true if tile n is a spike and false otherwise *)
